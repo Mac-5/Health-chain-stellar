66137675cb905f7d785c9c5d422b85172dea2fda
InheritX Developer <emmanuelokanandu99@gmail.com>
2026-02-21 19:10:23 +0100

    Initial commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlockchainModule } from './blockchain/blockchain.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+      },
+    }),
+    BlockchainModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
